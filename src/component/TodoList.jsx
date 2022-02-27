fd4d5dfc1eada114729c15cd602a13556626d1e4
macmeals <loseworld9731@gmail.com>
2022-02-27 19:11:07 +0900

    Todo登録の途中

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom"
 
-export const TodoList = () => {
+export const TodoList = (props) => {
+  const { todoItems } = props
   return (
     <div>
       <h2>Todo一覧</h2>
+      <ul>
+        {todoItems.map((todos) => {
+          return (
+            <li key={todos.id}>
+              <p className={todos.completeFlag ? "yokosen" : ""}>
+                {todos.todo}
+              </p>
+              {/* <button onClick={() => onCompleteTodo(index)}>完了</button>
+              <button onClick={() => onDeleteTodo(index)}>削除</button> */}
+            </li>
+          )
+        })}
+      </ul>
+
       <Link to="/todoregister">Todo登録</Link>
     </div>
   )
