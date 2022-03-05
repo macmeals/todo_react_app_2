b31bbb77e1de4bd846a8532085a28b73b0f4e03d
macmeals <loseworld9731@gmail.com>
2022-03-05 19:05:10 +0900

    todo登録した後、todoリストを登録

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,19 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
-export const TodoList = (props) => {
-  const { todoItems } = props
+export const TodoList = () => {
+  const { state } = useLocation()
+
   return (
     <div>
       <h2>Todo一覧</h2>
       <ul>
-        {todoItems.map((todos) => {
+        {state.state.map((todos) => {
           return (
             <li key={todos.id}>
               <p className={todos.completeFlag ? "yokosen" : ""}>
                 {todos.todo}
               </p>
-              {/* <button onClick={() => onCompleteTodo(index)}>完了</button>
-              <button onClick={() => onDeleteTodo(index)}>削除</button> */}
             </li>
           )
         })}
--- src/component/TodoRegister.jsx
+++ src/component/TodoRegister.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
-import { TodoList } from "../component/TodoList"
+// import { TodoList } from "../component/TodoList"
 
 export const TodoRegister = () => {
   //  初期値valueを空にセット、状態を格納する変数setValueをセット
@@ -8,6 +8,8 @@ export const TodoRegister = () => {
 
   // 初期値incompTodosにオブジェクト型の空配列をセット、状態をsetIncompleteTodosに格納する
   const [incompleteTodos, setIncompleteTodos] = useState([])
+
+  const [messeage, setMesseage] = useState("")
 
   // テキストボックスで入力した値を保存する
   const changeValue = (e) => setNewTodo(e.target.value)
@@ -20,6 +22,7 @@ export const TodoRegister = () => {
     ]
     setIncompleteTodos(newTodos) // setIncompleteTodosにnewTodosの状態を登録
     setNewTodo("") // setNewTodoに空の状態を登録
+    setMesseage("Todoを登録しました。")
   }
 
   return (
@@ -27,8 +30,11 @@ export const TodoRegister = () => {
       <h2>Todo登録</h2>
       <input type="text" value={newTodo} onChange={changeValue} />
       <button onClick={onAddTodo}>登録</button>
-      <TodoList todoItems={incompleteTodos} />
-      <Link to="/todolist">Todo一覧へ</Link>
+      {/* <TodoList todoItems={incompleteTodos} /> */}
+      <Link to={"/todolist"} state={{ state: incompleteTodos }}>
+        Todo一覧へ
+      </Link>
+      <p>{messeage}</p>
     </div>
   )
 }
