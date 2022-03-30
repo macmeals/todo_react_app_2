ab6176025fbbb140debed03446f42c9d4e943d67
macmeals <loseworld9731@gmail.com>
2022-03-30 22:52:25 +0900

    Topページの画像をポケモンAPIから取得

--- src/component/TopPage.jsx
+++ src/component/TopPage.jsx
@@ -6,8 +6,11 @@ import { Link } from "react-router-dom"
 import { css } from "@emotion/react"
 import DayPicker from "react-day-picker"
 import "react-day-picker/lib/style.css"
+import axios from "axios"
+import { useState } from "react"
 
 export const TopPage = () => {
+  const [image, setImage] = useState([])
   const topStyle = css`
     display: flex;
     justify-content: center;
@@ -15,7 +18,19 @@ export const TopPage = () => {
     align-items: center;
   `
 
-  console.log("TopPageのレンダリング")
+  const pikaImg = async () => {
+    try {
+      // ポケモンAPIからピカチュウの情報をaxiosで取得
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/25")
+      // ポケモンAPIのピカチュウの画像をStateで保存
+      setImage(response.data.sprites.front_default)
+    } catch {
+      console.log("ピカチュウの画像が取れませんでした")
+    }
+  }
+  pikaImg()
+
+  // console.log("TopPageのレンダリング")
 
   return (
     <div>
@@ -23,6 +38,7 @@ export const TopPage = () => {
         <h1>Todoアプリ</h1>
         <Link to="/todoregister">Todo登録</Link>
         <DayPicker />
+        <img src={image} alt="ピカチュウ画像" />
       </div>
     </div>
   )
